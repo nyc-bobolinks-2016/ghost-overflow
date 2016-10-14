510001d758c89353f25866180e45714b66669498
Olivia Coffaro & Paul Duca & Derek Yang <pair+oliviacoffaro.thepaulduca.tofumaker@devbootcamp.com>
2016-10-14 15:42:01 -0400

    derek sounds dumb

--- public/js/application.js
+++ public/js/application.js
@@ -11,4 +11,63 @@ $(document).ready(function() {
        alert(errorThrown)
     })
   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.new_comment_button').submit(function(){
+  event.preventDefault();
+  var route = $(this).attr('action');
+  var $parent = $(this).parent();
+  $.ajax({
+    url: route,
+    method: 'get'
+  }).done(function(response){
+    $parent.find('.new_comment_form').html(response);
+    $parent.find('.new_comment_button').hide();
+  })
+})
+
+
+$(".new_comment_form").submit(function(){
+  event.preventDefault();
+  var route = $(this).children().children().attr('action');
+  console.log(this);
+  $.ajax({
+    url: route,
+    method: 'post',
+    data: $(this).find('form').serialize()
+  }).done(function(response){
+    $(".comments_container").append(response)
+    $()
+  })
+})
+
+
+
+
+
+
+
 });
