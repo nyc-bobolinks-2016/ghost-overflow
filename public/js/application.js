cab4fee33038948702a3d127a2154192f6a82b3f
Derek Yang <dyy2105@columbia.edu>
2016-10-13 21:52:56 -0400

    Ajaxify the registration form to throw an alert. Make cosmetic changes to the readme file.

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+  $('#registration_form').submit(function(event){
+    event.preventDefault()
+    $.ajax({
+      method: 'post',
+      url: '/users/new',
+      data: $(this).serialize()
+    }).done(function(){
+      window.location.href = '/'
+    }).fail(function(jqXHR, textStatus, errorThrown){
+       alert(errorThrown)
+    })
+  })
 });
